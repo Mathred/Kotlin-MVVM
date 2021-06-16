3ad9d99ff51ef5f90bcbb31de8b98fafef814e3d
Mathred <temhbluturpa@gmail.com>
2021-06-16 23:47:30 +0300

    Initial commit + base impl of observer and VM

---
+++ app/src/main/java/com/example/kotlinmvvm/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinmvvm.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
