2984dcd01267cb471434036fd76b80e4b4cb8217
Mathred <temhbluturpa@gmail.com>
2021-06-20 17:06:09 +0300

    Added ActorList.kt entity Changed repository logic Changed viewModel logic

--- app/src/main/java/com/example/kotlinmvvm/model/entities/Movie.kt
+++ app/src/main/java/com/example/kotlinmvvm/model/entities/Movie.kt
@@ -1,13 +1,10 @@
 package com.example.kotlinmvvm.model.entities
 
 data class Movie(
+    val id: Int = 0,
     val name: String = "Die hard",
     val rating: Int = 77,
-    val poster: Int?,
+    val poster: Int? = null,
     val yearReleased: Int = 1988,
-    val cast: List<Actor>?
-) {
-    companion object {
-        fun getDefaultMovie() = Movie("Die Hard", 77,null,1988, null)
-    }
-}
+    val cast: List<Actor>? = null
+)
--- app/src/main/java/com/example/kotlinmvvm/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/kotlinmvvm/model/repository/RepositoryImpl.kt
@@ -1,13 +1,32 @@
 package com.example.kotlinmvvm.model.repository
 
+import com.example.kotlinmvvm.model.entities.ActorList
+import com.example.kotlinmvvm.model.entities.Category
 import com.example.kotlinmvvm.model.entities.Movie
 
 class RepositoryImpl : Repository {
-    override fun getDataFromServer(): Movie {
-        return Movie.getDefaultMovie()
+
+    override fun getMovieFromServer(id: Int?): Movie {
+        return Movie()
     }
 
-    override fun getDataFromLocalStorage(): Movie {
-        return Movie.getDefaultMovie()
+    override fun getMovieFromLocalStorage(id: Int?): Movie {
+        return Movie()
+    }
+
+    override fun getMovieListFromServer(id: Int?): Category {
+        return Category()
+    }
+
+    override fun getMovieListFromLocalStorage(id: Int?): Category {
+        return Category()
+    }
+
+    override fun getActorListFromServer(id: Int?): ActorList {
+        return ActorList()
+    }
+
+    override fun getActorListFromLocalStorage(id: Int?): ActorList {
+        return ActorList()
     }
 }
