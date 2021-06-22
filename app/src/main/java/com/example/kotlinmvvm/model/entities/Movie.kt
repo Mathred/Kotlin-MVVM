9a7faab8cc5082c0c2831402817ce949a9a60b96
Mathred <temhbluturpa@gmail.com>
2021-06-22 17:00:37 +0300

    Revert commit

--- app/src/main/java/com/example/kotlinmvvm/model/entities/Movie.kt
+++ app/src/main/java/com/example/kotlinmvvm/model/entities/Movie.kt
@@ -1,10 +1,13 @@
 package com.example.kotlinmvvm.model.entities
 
 data class Movie(
-    val id: Int = 0,
     val name: String = "Die hard",
     val rating: Int = 77,
-    val poster: Int? = null,
+    val poster: Int?,
     val yearReleased: Int = 1988,
-    val cast: List<Actor>? = null
-)
+    val cast: List<Actor>?
+) {
+    companion object {
+        fun getDefaultMovie() = Movie("Die Hard", 77,null,1988, null)
+    }
+}
